458bbdf8369317e13a5768550ee973ac88d4a00c
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-02-20 17:50:26 +0530

    Mainmenu with gamemanager and ui manger with other canvas

---
+++ Under the Caves/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
