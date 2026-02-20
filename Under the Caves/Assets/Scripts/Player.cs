31c3de495f30d61de7c2d4704982f341d164992e
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-02-20 09:51:10 +0530

    Player movement, jump and flip logic

---
+++ Under the Caves/Assets/Scripts/Player.cs
@@ -0,0 +1,60 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private int facingDirection = 1;
+
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb;
+
+    [Header("Input Settings")]
+    [SerializeField] private float movement;
+
+    [Header("Jump Settings")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float checkRadius;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private bool isGrounded;
+
+    void Update()
+    {
+        movement = Input.GetAxisRaw("Horizontal");
+
+        if (movement > 0 && transform.localScale.x < 0  || movement < 0 && transform.localScale.x > 0)
+        {
+            Flip();
+        }
+
+        Jump();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        rb.linearVelocity = new Vector2(movement * speed, rb.linearVelocity.y);
+    }
+
+    void Flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+
+    void Jump()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
+        
+        if(isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+}
